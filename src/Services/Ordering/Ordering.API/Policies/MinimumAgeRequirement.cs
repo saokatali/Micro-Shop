e9d706cba587a186de6fae9ac8aa470965df59d4
Saokat Ali <saokatali@outlook.com>
2022-02-04 13:12:20 +0530

    Updated to .net 6

--- src/Services/Ordering/Ordering.API/Policies/MinimumAgeRequirement.cs
+++ src/Services/Ordering/Ordering.API/Policies/MinimumAgeRequirement.cs
@@ -17,18 +17,17 @@ namespace Ordering.API.Policies
     {
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MinimumAgeRequirement requirement)
         {
-            if (!context.User.HasClaim(c => c.Type == "Age"))
+            if (context.User.HasClaim(c => c.Type.Equals("Age")))
             {
+                var age = int.Parse(context.User.FindFirst(c => c.Type.Equals("Age")).Value);
+
+
+                if (age >= requirement.MinimumAge)
+                {
+                    context.Succeed(requirement);
+                }
 
                 return Task.CompletedTask;
-            }
-
-            var age = int.Parse(context.User.FindFirst(c => c.Type.Equals("Age")).Value);
-
-
-            if (age >= requirement.MinimumAge)
-            {
-                context.Succeed(requirement);
             }
 
             return Task.CompletedTask;
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,20 +1,91 @@
+using Common.Web.Middleware;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Ordering.API.Common;
+using Ordering.API.Infrastructure;
+using Ordering.API.Policies;
+using System;
+using System.Reflection;
+using System.Text;
 
-namespace Ordering.API
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+IServiceCollection services = builder.Services;
+
+services.AddOptions();
+services.Configure<AppSettings>(builder.Configuration);
+services.AddHttpContextAccessor();
+services.AddDbContext<DataContext>(ServiceLifetime.Transient);
+services.AddMediatR(Assembly.GetExecutingAssembly());
+services.AddAutoMapper(Assembly.GetExecutingAssembly());
+services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opions =>
 {
-    public class Program
+    opions.RequireHttpsMetadata = true;
+    opions.SaveToken = true;
+    opions.TokenValidationParameters = new TokenValidationParameters
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        ValidateIssuer = false,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ValidateAudience = false,
+        ClockSkew = TimeSpan.Zero,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecretKey"]))
+    };
+});
+services.AddAuthorization(options =>
+{
+    options.AddPolicy("AtLeast18", policy =>
+    {
+        policy.Requirements.Add(new MinimumAgeRequirement(18));
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    options.AddPolicy("AdminRole", policy =>
+    {
+        policy.RequireClaim("Role", "Admin");
+    });
+});
+
+services.AddControllers(options =>
+{
+    options.Filters.Add(new AuthorizeFilter());
+});
+
+services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "Order API", Version = "v1" }));
+
+// Configure the HTTP request pipeline.
+
+var app = builder.Build();
+var env = app.Environment;
+app.UseException();
+
+if (!env.IsDevelopment())
+{
+    app.UseHsts();
+    app.UseHttpsRedirection();
 }
+if (env.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.RoutePrefix = string.Empty;
+        // string swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API");
+    });
+}
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
